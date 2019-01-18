9131afa0b838347091feba32730cf7337cccdf67
ohager <oliver@devbutze.com>
2019-01-17 23:22:45 -0200

    Fix/http mock (#26)
    * fixed http mock
    
    * added url mapping to http mock
    
    * added one more unit test

--- lib/packages/http/__tests__/githubApi.spec.ts
+++ lib/packages/http/__tests__/githubApi.spec.ts
@@ -1,19 +1,50 @@
-
-import Github from '../src/github';
+// Import HttpMock as first!
 import HttpMock from './helpers/httpMock';
+//
+import Github from '../src/github';
 
 /**
  * This test is a reference implementation to show how Http requests can be mocked
  */
 describe('Github', () => {
 
+    beforeEach(() => {
+        HttpMock.reset();
+    });
+
     it('should getAllPhoenixContributors easily', async () => {
 
-        HttpMock.onGet().reply(200, [{login: 'ohager'}]);
+        // mock for any get
+        HttpMock.onGet().reply(200, [{login: 'foo'}, {login: 'bar'}]);
         const github = new Github();
         const contributors = await github.getAllPhoenixContributors();
         expect(contributors.length).toBe(2);
-        expect(contributors).toEqual(['ohager', 'blankey1337']);
+        expect(contributors).toEqual(['foo', 'bar']);
+    });
+
+
+    it('should getAllPhoenixContributors - specific url mock', async () => {
+
+        // mock for that very specific endpoint
+        HttpMock.onGet('/phoenix/contributors').reply(200, [{login: 'baz'}]);
+        const github = new Github();
+        const contributors = await github.getAllPhoenixContributors();
+        expect(contributors.length).toBe(1);
+        expect(contributors).toEqual(['baz']);
+    });
+
+    it('should getAllPhoenixContributors - specific url mock , but non-existing', async () => {
+
+        // mock for that very specific endpoint
+        HttpMock.onGet('/phoenix/contributors/invalid').reply(200, [{login: 'baz'}]);
+        const github = new Github();
+        try {
+            await github.getAllPhoenixContributors();
+            expect('Should throw exception').toBeTruthy();
+        } catch (e) {
+            expect(e.message)
+                .toBe('No mock for called endpoint \'/phoenix/contributors\' found. Use onGet() or onGet(\'/phoenix/contributors\').');
+        }
     });
 
 
@@ -24,6 +55,6 @@ describe('Github', () => {
             await github.getAllPhoenixContributors();
         } catch (e) {
             expect(e).toBe('Oh no, internal server error. Blame the backend');
-        } 
+        }
     });
 });
