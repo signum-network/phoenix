758cf4165653dee771beb8356af9c01e3619a99b
ohager <oliver@devbutze.com>
2019-01-20 23:19:59 -0200

    documented the burst url method

--- lib/packages/core/src/service/baseService.ts
+++ lib/packages/core/src/service/baseService.ts
@@ -15,7 +15,16 @@ class BaseService {
         return this._http;
     }
 
-    public static toBRSUrl(method: string, data: any = {}): string {
+    /**
+     * Mounts a BRS conform API endpoint of format `<host>?requestType=getBlock&height=123`
+     *
+     * @see https://burstwiki.org/wiki/The_Burst_API
+     *
+     * @param method The method name for `requestType`
+     * @param data A JSON object which will be mapped to url params, e.g. {foo:1; bar:true} -> foo=1&bar=true
+     * @return The mounted url (without host)
+     */
+    public static toBRSEndpoint(method: string, data: any = {}): string {
         const request = `?requestType=${method}`;
         const params = Object.keys(data).map(k => `${k}=${data[k]}`).join('&');
 
