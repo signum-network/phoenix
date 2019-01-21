b2c0fe8d8acee5376d84415dfa8098da308cc4ec
Titanishu <titanishu@bk.ru>
2019-01-21 14:46:24 +0300

    Remove react-native-passcode-auth

--- mobile/src/core/utils/keychain.ts
+++ mobile/src/core/utils/keychain.ts
@@ -1,12 +1,9 @@
 import * as Keychain from 'react-native-keychain';
-// There is no types for react-native-passcode-auth
-// TODO: write them
-import PasscodeAuth from 'react-native-passcode-auth';
 import TouchID, { TouchIDError } from 'react-native-touch-id';
 import { IKeychainCredentials, ITouchIDOptionalConfig } from '../interfaces';
 import { isIOS } from './platform';
 
-function isFallbackTouchIDError (e: TouchIDError) {
+export function isFallbackTouchIDError (e: TouchIDError) {
   const { name } = e;
   return (
     name === 'LAErrorUserFallback' ||
@@ -20,39 +17,12 @@ export function setCredentials ({ username, password }: IKeychainCredentials): P
   return Keychain.setGenericPassword(username, password);
 }
 
-export async function requireAuth (reason: string) {
-  try {
-    await authWithTouchId(reason);
-  } catch (touchIDError) {
-    if (isFallbackTouchIDError(touchIDError)) {
-      // try to auth with passcode if it's fallback error or touchID isn't supported
-      try {
-        await authWithPassCode(reason);
-      } catch (passcodeError) {
-        throw passcodeError;
-      }
-    } else {
-      throw touchIDError;
-    }
-  }
-}
-
-export async function getCredentials (reason: string, withoutAuth?: boolean)
+export function getCredentials ()
   : Promise<boolean | IKeychainCredentials> {
-  if (!withoutAuth) {
-    try {
-      await requireAuth(reason);
-    } catch (e) {
-      throw e;
-    }
-
-    return Keychain.getGenericPassword();
-  } else {
-    return Keychain.getGenericPassword();
-  }
+  return Keychain.getGenericPassword();
 }
 
-function authWithTouchId (reason: string, optionalConfig?: ITouchIDOptionalConfig)
+export function authWithTouchId (reason: string, optionalConfig?: ITouchIDOptionalConfig)
   : Promise<boolean | TouchIDError> {
   const config: ITouchIDOptionalConfig = {
     fallbackTitle: '',
@@ -60,8 +30,4 @@ function authWithTouchId (reason: string, optionalConfig?: ITouchIDOptionalConfi
     ...optionalConfig
   };
   return TouchID.authenticate(reason, config);
-}
-
-function authWithPassCode (reason: string): Promise<boolean | any> {
-  return PasscodeAuth.authenticate(reason);
 }
