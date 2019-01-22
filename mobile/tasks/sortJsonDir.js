5dbdff4efb2e54c3b27ad891bd0ace6af0983ab0
Titanishu <titanishu@bk.ru>
2019-01-22 10:25:59 +0300

    Added translations sort tool,sorted translations

---
+++ mobile/tasks/sortJsonDir.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+const [jsonPath] = process.argv.slice(2);
+const sortJSON = require('sort-json');
+
+const walk = async (dir, filelist = []) => {
+  const files = await fs.readdir(dir);
+
+  for (file of files) {
+    const filepath = path.join(dir, file);
+    const stat = await fs.stat(filepath);
+
+    if (stat.isDirectory()) {
+      filelist = await walk(filepath, filelist);
+    } else if (filepath.indexOf('.json') !== -1) {
+      filelist.push(filepath);
+    }
+  }
+
+  return filelist;
+};
+
+walk(jsonPath).then(response => {
+  sortJSON.overwrite(response);
+});
