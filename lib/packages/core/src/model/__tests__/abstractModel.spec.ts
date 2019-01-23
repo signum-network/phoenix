edac51b5a5766d9863b26046e8500f8076139218
ohager <oliver@devbutze.com>
2019-01-22 22:16:03 -0200

    toJSON for abstract model

---
+++ lib/packages/core/src/model/__tests__/abstractModel.spec.ts
@@ -0,0 +1,83 @@
+import AbstractModel from '../abstractModel';
+
+describe('AbstractModel', () => {
+
+    class SimpleModel extends AbstractModel {
+        public foo: string;
+        public bar: number;
+        public cat: boolean;
+        public zombie: any;
+
+        constructor(obj: any) {
+            super();
+            this.mapJsonToProps(obj);
+        }
+    }
+
+    class ComplexModel extends AbstractModel {
+        public data: string;
+        public innerModel: SimpleModel;
+
+        constructor(obj: any) {
+            super();
+            this.mapJsonToProps(obj);
+        }
+    }
+
+
+    describe('Simple Model', () => {
+        const simple = new SimpleModel({
+                foo: 'some text',
+                bar: 1,
+                cat: true
+            }
+        );
+
+        it('should map fields according the passed object', () => {
+            expect(simple.foo).toBe('some text');
+            expect(simple.bar).toBe(1);
+            expect(simple.cat).toBeTruthy();
+            expect(simple.zombie).toBeUndefined();
+        });
+
+        it('should return Json on toJson()', () => {
+            const json = simple.toJson();
+            expect(json.foo).toBe('some text');
+            expect(json.bar).toBe(1);
+            expect(json.cat).toBeTruthy();
+            expect(json.zombie).toBeUndefined();
+        });
+    });
+
+    describe('Complex Model', () => {
+        const complex = new ComplexModel({
+            data: 'data text',
+            innerModel : {
+                    foo: 'some text',
+                    bar: 1,
+                    cat: true
+                }
+            }
+        );
+
+        it('should map fields according the passed object', () => {
+            expect(complex.data).toBe('data text');
+            // note that these are only shallow copies
+            expect(complex.innerModel.foo).toBe('some text');
+            expect(complex.innerModel.bar).toBe(1);
+            expect(complex.innerModel.cat).toBeTruthy();
+            expect(complex.innerModel.zombie).toBeUndefined();
+        });
+
+        it('should return Json on toJson()', () => {
+            const json = complex.toJson();
+            expect(json.data).toBe('data text');
+            // note that these are only shallow copies
+            expect(complex.innerModel.foo).toBe('some text');
+            expect(complex.innerModel.bar).toBe(1);
+            expect(complex.innerModel.cat).toBeTruthy();
+            expect(complex.innerModel.zombie).toBeUndefined();
+        });
+    });
+
+});
--- lib/packages/core/src/model/abstractModel.ts
+++ lib/packages/core/src/model/abstractModel.ts
@@ -3,4 +3,12 @@ export default abstract class AbstractModel {
     protected mapJsonToProps(json: any) {
         Object.keys(json).forEach(k => this[k] = json[k]);
     }
+
+    /**
+     * Transform properties of this class into an JSON object
+     * @param customMapper A custom mapping function to override the default mapping
+     */
+    public toJson(customMapper?: (thisModel) => any): any {
+        return customMapper ? customMapper(this) : {...this};
+    }
 }
