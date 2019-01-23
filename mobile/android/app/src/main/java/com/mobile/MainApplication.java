68c9372788918eb39c1bd0a3e8bd7829c4768146
Titanishu <Titanishu@bk.ru>
2019-01-23 15:01:34 +0300

    Internationalization (#35)
    * Added internationalization
    
    * Added translations sort tool,sorted translations

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rnlanguages.RNLanguagesPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLanguagesPackage(),
             new FingerprintAuthPackage(),
             new KeychainPackage(),
             new RNGestureHandlerPackage()
