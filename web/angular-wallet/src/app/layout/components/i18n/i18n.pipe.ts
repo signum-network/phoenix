e733a634061bf5ef12ce47e03c65a64541708882
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-02-04 21:34:43 -0800

    chore: remove console.log

--- web/angular-wallet/src/app/layout/components/i18n/i18n.pipe.ts
+++ web/angular-wallet/src/app/layout/components/i18n/i18n.pipe.ts
@@ -10,7 +10,6 @@ export class I18nPipe implements PipeTransform {
   constructor(public i18nService: I18nService){}
 
   transform(phrase: any, args?: any): any {
-    console.log(phrase);
     return this.i18nService.getTranslation(phrase);
   }
 
