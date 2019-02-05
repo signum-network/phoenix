d04640b718eda9f20251ee64b28ae12d2024af51
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-02-04 19:37:31 -0800

    test(web): remove unnecessary mappings

--- web/angular-wallet/jest.config.js
+++ web/angular-wallet/jest.config.js
@@ -3,13 +3,5 @@ module.exports = {
   preset: 'jest-preset-angular',
   roots: ['src'],
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  moduleNameMapper: {
-    '@app/(.*)': '<rootDir>/src/app/$1',
-    '@assets/(.*)': '<rootDir>/src/assets/$1',
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-    '@env': '<rootDir>/src/environments/environment',
-    '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1'
-  },
   transformIgnorePatterns: ['node_modules/(?!(jest-test))']
 };
