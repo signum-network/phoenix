7813ffb9679200deb7bdc07c096b73e0bfa7f490
ohager <oliver.hager@dextra-sw.com>
2019-02-13 22:26:28 -0200

    fixed ng wallet build error

--- scripts/ci/web.sh
+++ scripts/ci/web.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd web/angular-wallet
+# pre-built lib to resolve deps
+cd lib
+npm install
+npm run bootstrap
+
+# run angular build
+cd ../web/angular-wallet
 npm install
 npm run test
 npm run build:ci
