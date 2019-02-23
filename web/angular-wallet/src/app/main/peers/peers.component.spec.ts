487537720e40dbea272326bcb34eed2049597db0
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-02-23 12:37:36 -0800

    test(peers): update tests

--- web/angular-wallet/src/app/main/peers/peers.component.spec.ts
+++ web/angular-wallet/src/app/main/peers/peers.component.spec.ts
@@ -1,6 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSortModule, MatTableModule, MatPaginatorModule  } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { I18nModule } from 'app/layout/components/i18n/i18n.module';
+import { BehaviorSubject } from 'rxjs';
+import { StoreService } from 'app/store/store.service';
+import { I18nService } from 'app/layout/components/i18n/i18n.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PeersComponent } from './peers.component';
+import { NetworkService } from 'app/network/network.service';
+import { TimeAgoPipe } from 'time-ago-pipe';
+import { NotifierModule } from 'angular-notifier';
+import { PeersResolver } from './peers.resolver';
 
 describe('PeersComponent', () => {
   let component: PeersComponent;
@@ -8,6 +21,42 @@ describe('PeersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatSortModule,
+        MatTableModule,
+        MatIconModule,
+        MatButtonModule,
+        MatPaginatorModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule, 
+        I18nModule,
+        BrowserAnimationsModule,
+        NotifierModule,
+        RouterTestingModule.withRoutes(
+          [{path: 'peers', component: PeersComponent}]
+        )
+      ],
+      providers: [ I18nService, {
+        provide: StoreService,
+        useFactory: () => {
+          return {
+            ready: new BehaviorSubject(true),
+            getSettings: () => Promise.resolve({language: 'en'}),
+            saveSettings: () => Promise.resolve(true)
+          };
+        }
+      },
+      {
+        provide: NetworkService,
+        useFactory: () => {
+          return {
+            getPeers: () => Promise.resolve({peers: ['1.2.3.4']}),
+            getPeer: () => Promise.resolve({announcedAddress: '1.2.3.4'})
+          }
+        }
+      }],
       declarations: [ PeersComponent ]
     })
     .compileComponents();
