e2ad0a4d20ce40d8624c38dfd03c0bc3c9f16293
ohager <oliver.hager@dextra-sw.com>
2019-03-07 21:30:01 -0300

    getAccountIdFromPublicKey works fine without BN

--- lib/packages/crypto/src/getAccountIdFromPublicKey.ts
+++ lib/packages/crypto/src/getAccountIdFromPublicKey.ts
@@ -1,11 +1,8 @@
 /** @module crypto */
 
-import { Converter } from './converter';
+import {Converter} from './converter';
 import * as CryptoJS from 'crypto-js';
-
-// TODO: reduce the bignumber stuff even more...just what we need!
-import {BigNumber} from './bignumber';
-
+import {Big} from 'big.js';
 
 /**
  * Convert hex string of the public key to the account id
@@ -15,10 +12,10 @@ import {BigNumber} from './bignumber';
 export const getAccountIdFromPublicKey = (publicKey: string): string => {
     const hash = CryptoJS.SHA256(CryptoJS.enc.Hex.parse(publicKey));
     const bytes = Converter.convertWordArrayToByteArray(hash);
-    const slice = bytes.slice(0, 8).reverse();
-
-    // const result = slice.reduce( (acc, num, index) => acc + num * Math.pow(2, 56-(index*8) ), 0);
-    // return result + 39;
-    const id = new BigNumber(slice, 256); // base 256 for byte
-    return id.toString();
+    const slice = bytes.slice(0, 8);
+    const result = slice.reduce(
+        (acc, num, index) => acc.add(Big(num).mul(Big(2).pow(index * 8))),
+        Big(0)
+    );
+    return result.toFixed();
 };
