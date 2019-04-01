de116f91a5d38a3c81b4baabf14f0a67dee2f08a
ohager <oliver.hager@dextra-sw.com>
2019-04-01 08:17:33 -0300

    sendEncryptedTextMessage implemented

---
+++ lib/packages/core/src/api/factories/message/sendEncryptedTextMessage.ts
@@ -0,0 +1,55 @@
+/** @ignore */
+/** @module core */
+
+/**
+ * Copyright (c) 2019 Burst Apps Team
+ */
+import {BurstService} from '../../../burstService';
+import {TransactionId} from '../../../typings/transactionId';
+import {TransactionResponse} from '../../../typings/transactionResponse';
+import {generateSignature} from '@burstjs/crypto';
+import {verifySignature, generateSignedTransactionBytes, encryptMessage} from '@burstjs/crypto';
+import {convertNumberToNQTString} from '@burstjs/util';
+import {broadcastTransaction} from '../transaction/broadcastTransaction';
+
+export const sendEncryptedTextMessage = (service: BurstService):
+    (message: string,
+     recipientId: string,
+     recipientPublicKey: string,
+     senderPublicKey: string,
+     senderPrivateKey: string,
+     fee?: number) => Promise<TransactionId> =>
+    async (
+        message: string,
+        recipientId: string,
+        recipientPublicKey: string,
+        senderPublicKey: string,
+        senderPrivateKey: string,
+        fee: number = 0.1,
+    ): Promise<TransactionId> => {
+
+        const encryptedMessage = encryptMessage(message, recipientPublicKey, senderPrivateKey);
+
+        // todo implement a message limit to be checked
+
+        const parameters = {
+            recipient: recipientId,
+            publicKey: senderPublicKey,
+            encryptedMessageData: encryptedMessage.data,
+            encryptedMessageNonce: encryptedMessage.nonce,
+            encryptedMessageIsText: true,
+            broadcast: true,
+            deadline: 1440, // which deadline?
+            feeNQT: convertNumberToNQTString(fee),
+        };
+
+
+        const {unsignedTransactionBytes: unsignedHexMessage} = await service.send<TransactionResponse>('sendMessage', parameters);
+        const signature = generateSignature(unsignedHexMessage, senderPrivateKey);
+        if (!verifySignature(signature, unsignedHexMessage, senderPublicKey)) {
+            throw new Error('The signed message could not be verified! Message not broadcasted!');
+        }
+
+        const signedMessage = generateSignedTransactionBytes(unsignedHexMessage, signature);
+        return broadcastTransaction(service)(signedMessage);
+    };
