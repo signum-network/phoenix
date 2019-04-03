69196e87d351fc7e03838d38630f1f12cee492d0
ohager <oliver.hager@dextra-sw.com>
2019-04-03 14:56:48 -0300

    update API

--- lib/packages/core/src/typings/api/messageApi.ts
+++ lib/packages/core/src/typings/api/messageApi.ts
@@ -1,6 +1,7 @@
 /** @module core */
 
 import {TransactionId} from '../transactionId';
+import {Keys} from '@burstjs/crypto';
 
 /**
  * The Message API
@@ -18,6 +19,7 @@ export interface MessageApi {
      * @param recipientId The recipients Id, not RS Address
      * @param senderPublicKey The senders public key for sending an _unsigned_ message
      * @param senderPrivateKey The senders private key to _sign_ the message
+     * @param deadline The optional deadline (in minutes) for the transaction to be confirmed, 1440 minutes maximum
      * @param fee The optional fee (expressed in Burst) for the message, default is 0.1 Burst.
      * @return The Transaction Id
      */
@@ -26,6 +28,28 @@ export interface MessageApi {
         recipientId: string,
         senderPublicKey: string,
         senderPrivateKey: string,
+        deadline?: number,
+        fee?: number
+    ) => Promise<TransactionId>;
+
+
+    /**
+     * Broadcasts an _encrypted_ text message to the network/blockchain
+     * It's analogous to sendTextMessage but encrypts the message priorly
+     *
+     * @param message The _text_ message to be encrypted and sent
+     * @param recipientId The recipients Id, not RS Address
+     * @param keys The senders key set for encryption and signing
+     * @param deadline The optional deadline (in minutes) for the transaction to be confirmed, 1440 minutes maximum
+     * @param fee The optional fee (expressed in Burst) for the message, default is 0.1 Burst.
+     * @return The Transaction Id
+     */
+    sendEncryptedTextMessage: (
+        message: string,
+        recipientId: string,
+        recipientPublicKey: string,
+        senderKeys: Keys,
+        deadline?: number,
         fee?: number
     ) => Promise<TransactionId>;
 }
