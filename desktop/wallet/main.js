bf584cac9baa712e068733e1058aef99696a4c70
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-04-09 21:09:39 -0700

    fix(osx): add Help menu and fix CMC URL on OSX (#171)
    * fix(osx): add Help menu and fix CMC URL on OSX
    
    * chore: fix linting
    
    * chore: fix linting
    
    * chore: fix win32 error

--- desktop/wallet/main.js
+++ desktop/wallet/main.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, shell } = require('electron');
 
 let win;
 
@@ -10,18 +10,18 @@ function getBrowserWindowConfig() {
         icon: path.join(__dirname, 'assets/images/png/64x64.png')
     };
     return isDevelopment ? {
-        ...commonConfig ,
+        ...commonConfig,
         fullscreen: false,
         width: 800,
         height: 600
     } :
-    {
-        ...commonConfig,
-        fullscreen: true,
-    }
+        {
+            ...commonConfig,
+            fullscreen: true,
+        }
 }
 
-function createWindow () {
+function createWindow() {
 
     const distPath = path.join(__dirname, "./dist");
     // Create the browser window.
@@ -36,30 +36,114 @@ function createWindow () {
         win = null
     })
 
-    const template = [{
-        label: 'Phoenix',
-        submenu: [
-            { label: 'About Phoenix', selector: 'orderFrontStandardAboutPanel:' },
-            { type: 'separator' },
-            { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit(); }}
-        ]}, {
-        label: 'Edit',
-        submenu: [
-            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-            { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
-            { type: 'separator' },
-            { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-            { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-            { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-            { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
-        ]}
+    const template = [
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' }
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'toggledevtools' },
+                { type: 'separator' },
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            role: 'window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'close' }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Burst Wiki',
+                    click() { shell.openExternal('https://burstwiki.org/'); }
+                },
+                {
+                    label: 'Report An Issue',
+                    click() { shell.openExternal('https://github.com/burst-apps-team/phoenix/issues/new'); }
+                },
+                {
+                    label: 'Credits',
+                    selector: 'orderFrontStandardAboutPanel:'
+                }
+            ]
+        }
     ];
+
+    if (process.platform === 'darwin') {
+        template.unshift({
+            label: app.getName(),
+
+            submenu: [
+
+                { label: `About ${app.getName()}`, selector: 'orderFrontStandardAboutPanel:' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit', accelerator: 'Command+Q', click: () => { app.quit(); } }
+            ]
+        })
+
+        // Edit menu
+        template[1].submenu.push(
+            { type: 'separator' },
+            {
+                label: 'Speech',
+                submenu: [
+                    { role: 'startspeaking' },
+                    { role: 'stopspeaking' }
+                ]
+            }
+        );
+
+        // Window menu
+        template[3].submenu = [
+            { role: 'close' },
+            { role: 'minimize' },
+            { role: 'zoom' },
+            { type: 'separator' },
+            { role: 'front' }
+        ];
+    }
 
     Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 // Create window on electron intialization
 app.on('ready', createWindow);
+
+if (process.platform !== 'darwin') {
+    app.setAboutPanelOptions({
+        applicationName: app.getName(),
+        applicationVersion: app.getVersion(),
+        copyright: 'Burst Apps Team',
+        credits: 'ohager, blankey1337, Matheus Castiglioni',
+        version: process.versions.electron
+    });
+}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
