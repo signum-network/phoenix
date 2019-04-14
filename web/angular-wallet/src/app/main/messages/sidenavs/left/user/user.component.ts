6916a9c6bd3cd8d53641e5f170358e2651c5f5c8
ohager <oliver.hager@dextra-sw.com>
2019-04-14 09:26:23 -0300

    linting user.component

--- web/angular-wallet/src/app/main/messages/sidenavs/left/user/user.component.ts
+++ web/angular-wallet/src/app/main/messages/sidenavs/left/user/user.component.ts
@@ -1,88 +1,56 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
-import { MessagesService } from 'app/main/messages/messages.service';
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged, takeUntil} from 'rxjs/operators';
+import {MessagesService} from 'app/main/messages/messages.service';
 
 
 @Component({
-    selector     : 'message-user-sidenav',
-    templateUrl  : './user.component.html',
-    styleUrls    : ['./user.component.scss'],
-    encapsulation: ViewEncapsulation.None
+  selector: 'message-user-sidenav',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class MessageUserSidenavComponent implements OnInit, OnDestroy
-{
-    user: any;
-    userForm: FormGroup;
+export class MessageUserSidenavComponent implements OnInit, OnDestroy {
+  user: any;
+  userForm: FormGroup;
 
-    // Private
-    private _unsubscribeAll: Subject<any>;
+  private _unsubscribeAll: Subject<any>;
 
-    /**
-     * Constructor
-     *
-     * @param {MessagesService} _messageService
-     */
-    constructor(
-        private _messageService: MessagesService
-    )
-    {
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
-    }
+  constructor(
+    private _messageService: MessagesService
+  ) {
+    // Set the private defaults
+    this._unsubscribeAll = new Subject();
+  }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+  ngOnInit(): void {
+    this.user = this._messageService.user;
 
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        this.user = this._messageService.user;
+    this.userForm = new FormGroup({
+      mood: new FormControl(this.user.mood),
+      status: new FormControl(this.user.status)
+    });
 
-        this.userForm = new FormGroup({
-            mood  : new FormControl(this.user.mood),
-            status: new FormControl(this.user.status)
-        });
+    this.userForm.valueChanges
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(data => {
+        this.user.mood = data.mood;
+        this.user.status = data.status;
+      });
+  }
 
-        this.userForm.valueChanges
-            .pipe(
-                takeUntil(this._unsubscribeAll),
-                debounceTime(500),
-                distinctUntilChanged()
-            )
-            .subscribe(data => {
-                this.user.mood = data.mood;
-                this.user.status = data.status;
-                // this._messageService.updateUserData(this.user);
-            });
-    }
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next();
-        this._unsubscribeAll.complete();
-    }
-
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * Change left sidenav view
-     *
-     * @param view
-     */
-    changeLeftSidenavView(view): void
-    {
-        this._messageService.onLeftSidenavViewChanged.next(view);
-    }
+  changeLeftSidenavView(view): void {
+    this._messageService.onLeftSidenavViewChanged.next(view);
+  }
 
 }
