5c4117dd2bfe931b961997a96c457c4d62140d98
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-04-20 12:14:26 -0700

    chore: minor cleanup

--- web/angular-wallet/src/app/main/accounts/account-details/add-node-url.pipe.ts
+++ web/angular-wallet/src/app/main/accounts/account-details/add-node-url.pipe.ts
@@ -1,17 +1,12 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { ApiService } from "app/api.service";
-import { StoreService } from "app/store/store.service";
-import { Settings } from "app/settings";
 
 @Pipe({ name: 'addNodeUrl' })
 export class AddNodeUrlPipe implements PipeTransform {
 
     nodeUrl: string = '';
-    constructor(private storeService: StoreService, private apiService: ApiService) {
+    constructor(private apiService: ApiService) {
         this.nodeUrl = this.apiService.nodeUrl;
-        // this.storeService.settings.subscribe((settings: Settings) => {
-        //     this.nodeUrl = settings.node;
-        // });
     }
 
     transform(value: string): string {
