0f4cc4bbdc3abcc7f6289438f8070d7b2cc63978
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-04-25 16:53:26 -0700

    fix(app): fix imports

--- web/angular-wallet/src/app/layout/components/i18n/i18n.module.ts
+++ web/angular-wallet/src/app/layout/components/i18n/i18n.module.ts
@@ -1,9 +1,9 @@
-import {NgModule} from "app/layout/components/i18n/node_modules/@angular/core";
+import {NgModule} from "@angular/core";
 
 import {LanguageSelectorComponent} from "./language-selector/language-selector.component";
 import {I18nPipe} from "./i18n.pipe";
 import {I18nService} from "./i18n.service";
-import {CommonModule} from "app/layout/components/i18n/node_modules/@angular/common";
+import {CommonModule} from "@angular/common";
 
 export interface Language {
   name: string,
