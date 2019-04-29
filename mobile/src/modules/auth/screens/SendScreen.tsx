ac8a4b83055d2ed01b57644d155555a8183f9baa
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-04-28 17:21:01 -0700

    Feat/send burst mobile (#243)
    * feat(nav): scaffold app navigation
    
    * fix(nav): use different stacks
    
    * fix(nav): use different stacks
    
    * fix(Home): use var for safety
    
    * fix(app): fix imports
    
    * fix: address pr feedback
    
    * fix(mobile): add missing files
    
    * fix: address pr feedback
    
    * feat(send): start work on send burst form
    
    * fix(SendScreen): use pre-built components
    
    * fix(i18n): fix merge conflict

--- mobile/src/modules/auth/screens/SendScreen.tsx
+++ mobile/src/modules/auth/screens/SendScreen.tsx
@@ -1,46 +1,90 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Button, View } from 'react-native';
 import { NavigationInjectedProps, withNavigation } from 'react-navigation';
 import { connect } from 'react-redux';
-import { Text, TextThemes } from '../../../core/components/base/Text';
+import { Input, KeyboardTypes } from '../../../core/components/base/Input';
 import { HeaderTitle } from '../../../core/components/header/HeaderTitle';
 import { i18n } from '../../../core/i18n';
 import { InjectedReduxProps } from '../../../core/interfaces';
 import { FullHeightView } from '../../../core/layout/FullHeightView';
 import { Screen } from '../../../core/layout/Screen';
 import { ApplicationState } from '../../../core/store/initialState';
-import { Sizes } from '../../../core/theme/sizes';
 import { core } from '../../../core/translations';
 import { AuthReduxState } from '../store/reducer';
 
 interface IProps extends InjectedReduxProps {
-  auth: AuthReduxState,
+  auth: AuthReduxState;
 }
-type Props = IProps & NavigationInjectedProps;
 
-const styles = StyleSheet.create({
-  hintView: {
-    paddingTop: Sizes.SMALL,
-    flexGrow: 1
-  }
-});
+interface SendBurstFormState {
+  recipientAddress: string;
+  amountNQT: string;
+  feeNQT: string;
+  showMessage: boolean;
+  advanced: boolean;
+  message: string;
+  encrypt: boolean;
+  deadline: string;
+  pin: string;
+}
+
+type Props = IProps & NavigationInjectedProps;
 
 class Send extends React.PureComponent<Props> {
   static navigationOptions = {
     headerTitle: <HeaderTitle>{i18n.t(core.screens.send.title)}</HeaderTitle>
   };
 
+  state: SendBurstFormState = {
+    recipientAddress: '',
+    amountNQT: '0',
+    feeNQT: '0',
+    showMessage: false,
+    advanced: false,
+    message: '',
+    encrypt: true,
+    deadline: '24',
+    pin: ''
+  };
+
+  private handleSubmit = () => {
+    // console.log(this.state.recipientAddress, this.state.amountNQT, this.state.feeNQT);
+  }
+  private handleInputChange (key: keyof SendBurstFormState): ((text: string) => void) {
+    return (text) => this.setState({ [key]: text });
+  }
+  private handleBurstInputChange (): ((text: string) => void) {
+    return (text) => this.setState({ recipientAddress: text });
+  }
+
   render () {
     return (
       <Screen>
         <FullHeightView>
-          <View style={styles.hintView}>
-            <Text theme={TextThemes.HEADER}>
-              {i18n.t(core.screens.send.title)}
-            </Text>
-          </View>
           <View>
-            <Text>Send Burst Form</Text>
+            <Input
+              keyboardType={KeyboardTypes.EMAIL}
+              placeholder='BURST-____-____-____-_____'
+              onChangeText={this.handleBurstInputChange()}
+              value={this.state.recipientAddress}
+            />
+            <Input
+              keyboardType={KeyboardTypes.NUMERIC}
+              placeholder={i18n.t(core.screens.send.amountNQT)}
+              onChangeText={this.handleInputChange('amountNQT')}
+              value={this.state.amountNQT}
+            />
+            <Input
+              keyboardType={KeyboardTypes.NUMERIC}
+              placeholder={i18n.t(core.screens.send.feeNQT)}
+              onChangeText={this.handleInputChange('feeNQT')}
+              value={this.state.feeNQT}
+            />
+            <Button
+              title={i18n.t(core.actions.submit)}
+              onPress={this.handleSubmit}
+            />
+
           </View>
         </FullHeightView>
       </Screen>
