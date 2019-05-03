38084dec55530e7795eb23e152d08d8652aee816
ohager <oliver.hager@dextra-sw.com>
2019-05-03 10:14:49 -0300

    Fix/message component refinements (#265)
    * added icon also to new message contact
    
    * refactored dataflow of message options, added fee to toolbar, and nice button
    
    * fix build error

--- web/angular-wallet/src/app/main/messages/messages.component.ts
+++ web/angular-wallet/src/app/main/messages/messages.component.ts
@@ -1,75 +1,54 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import {Component, OnDestroy, OnInit, ViewEncapsulation, ViewChild} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
-import { fuseAnimations } from '@fuse/animations';
+import {fuseAnimations} from '@fuse/animations';
 
-import { MessagesService } from './messages.service';
-import { SuggestedFees } from '@burstjs/core/out';
-import { ActivatedRouteSnapshot, ActivatedRoute } from '@angular/router';
-import { MessageRightSidenavComponent } from './sidenavs/right/right.component';
-import { convertNQTStringToNumber } from '@burstjs/util/out';
+import {MessagesService} from './messages.service';
+import {SuggestedFees} from '@burstjs/core/out';
+import {ActivatedRoute} from '@angular/router';
+import {MessageRightSidenavComponent} from './sidenavs/right/right.component';
+import {convertNQTStringToNumber} from '@burstjs/util/out';
 
 @Component({
-    selector     : 'messages',
-    templateUrl  : './messages.component.html',
-    styleUrls    : ['./messages.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+  selector: 'messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
-export class MessagesComponent implements OnInit, OnDestroy
-{
-    selectedMessage: any;
-    fees: SuggestedFees;
-    encrypt: boolean;
-    feeNQT: number;
-    @ViewChild(MessageRightSidenavComponent) rightSidenav;
+export class MessagesComponent implements OnInit, OnDestroy {
+  @ViewChild(MessageRightSidenavComponent) rightSidenav;
 
-    // Private
-    private _unsubscribeAll: Subject<any>;
+  selectedMessage: any;
+  fees: SuggestedFees;
+  encrypt: boolean;
+  feeNQT: number;
 
-    /**
-     * Constructor
-     *
-     * @param {MessagesService} _messageService
-     */
-    constructor(
-        private _messageService: MessagesService,
-        private route: ActivatedRoute
-    )
-    {
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
+  private _unsubscribeAll: Subject<any>;
 
-        this.fees = this.route.snapshot.data.fees as SuggestedFees;
-        this.encrypt = true;
-        this.feeNQT = convertNQTStringToNumber(this.fees.standard.toString());
+  constructor(
+    private _messageService: MessagesService,
+    private route: ActivatedRoute
+  ) {
+    this._unsubscribeAll = new Subject();
 
-    }
+    this.fees = this.route.snapshot.data.fees as SuggestedFees;
+    this.encrypt = true;
+    this.feeNQT = convertNQTStringToNumber(this.fees.standard.toString());
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+  }
 
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        this._messageService.onMessageSelected
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(({message}) => {
-                this.selectedMessage = message;
-            });
-    }
+  ngOnInit(): void {
+    this._messageService.onMessageSelected
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(({message}) => {
+        this.selectedMessage = message;
+      });
+  }
 
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next();
-        this._unsubscribeAll.complete();
-    }
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 }
--- web/angular-wallet/src/app/main/messages/sidenavs/right/right.component.ts
+++ web/angular-wallet/src/app/main/messages/sidenavs/right/right.component.ts
@@ -1,66 +1,45 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation, Input, Output, ViewChild } from '@angular/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import {Component, OnDestroy, OnInit, ViewEncapsulation, Input, Output, ViewChild} from '@angular/core';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
-import { fuseAnimations } from '@fuse/animations';
+import {fuseAnimations} from '@fuse/animations';
 
-import { MessagesService } from '../../messages.service';
-import { SuggestedFees } from '@burstjs/core/out';
-import { MessageOptionsSidenavComponent } from './options/options.component';
+import {MessagesService} from '../../messages.service';
+import {SuggestedFees} from '@burstjs/core/out';
+import {MessageOptionsSidenavComponent} from './options/options.component';
 
 @Component({
-    selector     : 'message-right-sidenav',
-    templateUrl  : './right.component.html',
-    styleUrls    : ['./right.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+  selector: 'message-right-sidenav',
+  templateUrl: './right.component.html',
+  styleUrls: ['./right.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  animations: fuseAnimations
 })
-export class MessageRightSidenavComponent implements OnInit, OnDestroy
-{
-    view: string;
-    @Input('fees') fees: SuggestedFees;
-    @Input('feeNQT') feeNQT: string;
-    @Input('encrypt') encrypt: boolean;
-    @ViewChild(MessageOptionsSidenavComponent) options;
+export class MessageRightSidenavComponent implements OnInit, OnDestroy {
+  view: string;
+  @Input('fees') fees: SuggestedFees;
+  @ViewChild(MessageOptionsSidenavComponent) options;
 
-    // Private
-    private _unsubscribeAll: Subject<any>;
+  private _unsubscribeAll: Subject<any>;
 
-    constructor(
-        private _messageService: MessagesService
-    )
-    {
-        // Set the defaults
-        this.view = 'options';
+  constructor(
+    private _messageService: MessagesService
+  ) {
+    this.view = 'options';
+    this._unsubscribeAll = new Subject();
+  }
 
-        // Set the private defaults
-        this._unsubscribeAll = new Subject();
-    }
+  ngOnInit(): void {
+    this._messageService.onRightSidenavViewChanged
+      .pipe(takeUntil(this._unsubscribeAll))
+      .subscribe(view => {
+        this.view = view;
+      });
+  }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        this._messageService.onRightSidenavViewChanged
-            .pipe(takeUntil(this._unsubscribeAll))
-            .subscribe(view => {
-                this.view = view;
-            });
-    }
-
-    /**
-     * On destroy
-     */
-    ngOnDestroy(): void
-    {
-        // Unsubscribe from all subscriptions
-        this._unsubscribeAll.next();
-        this._unsubscribeAll.complete();
-    }
+  ngOnDestroy(): void {
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 
 }
