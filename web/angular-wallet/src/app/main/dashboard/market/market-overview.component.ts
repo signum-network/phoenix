be3857d8afa6c9355938a5c0801820359b677de5
ohager <oliver.hager@dextra-sw.com>
2019-05-04 01:47:10 -0300

    formatting market info widget

--- web/angular-wallet/src/app/main/dashboard/market/market-overview.component.ts
+++ web/angular-wallet/src/app/main/dashboard/market/market-overview.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {formatCurrency, formatPercent} from '@angular/common';
 import {MarketService} from './market.service';
-import {takeWhile} from 'rxjs/operators';
+import {takeUntil} from 'rxjs/operators';
 import {MarketTicker} from './types';
-import {I18nService} from '../../../layout/components/i18n/i18n.service';
+import {StoreService} from '../../../store/store.service';
+import {UnsubscribeOnDestroy} from '../../../util/UnsubscribeOnDestroy';
 
 class MarketTickerImpl implements MarketTicker {
   readonly '24h_volume_usd' = '';
@@ -33,21 +34,28 @@ class MarketTickerImpl implements MarketTicker {
   ]
 })
 
-export class MarketOverviewComponent implements OnInit, OnDestroy {
-  private _isActive = true;
-
+export class MarketOverviewComponent extends UnsubscribeOnDestroy implements OnInit{
   public isLoading = true;
   private tickerData: MarketTicker = new MarketTickerImpl();
   private locale: any;
 
-  constructor(private marketService: MarketService, private i18nService: I18nService) {
-    this.locale = i18nService.currentLanguage.code;
+  constructor(private marketService: MarketService, private storeService: StoreService) {
+    super();
+    this.storeService.settings
+      .pipe(
+        takeUntil(this.unsubscribeAll)
+      )
+      .subscribe(async ({language}) => {
+          this.locale = language;
+        }
+      );
+
   }
 
   public ngOnInit(): void {
     this.marketService.ticker$
       .pipe(
-        takeWhile(this.isActive)
+        takeUntil(this.unsubscribeAll)
       )
       .subscribe(tickerData => {
         this.isLoading = false;
@@ -55,23 +63,25 @@ export class MarketOverviewComponent implements OnInit, OnDestroy {
       });
   }
 
-  ngOnDestroy(): void {
-    this._isActive = false;
+  private asCurrency(value: string, digitsInfo: string = '1.2-2'): string {
+    return `${formatCurrency(parseFloat(value), this.locale, '', undefined, digitsInfo)}`;
   }
 
-  isActive = () => this._isActive;
+  public getPriceSats = (): string => {
+    const sats = (parseFloat(this.tickerData.price_btc) * 10 * 10e6).toFixed(0);
+    return `${this.asCurrency(sats, '1.0-6')} SATS`;
+  }
 
-  public getPriceBtc = (): string => `${this.tickerData.price_btc} BTC`;
-  public getPriceUsd = (): string => `${this.tickerData.price_usd} USD`;
-  public get24hVolume = (): string => `${formatCurrency(parseFloat(this.tickerData['24h_volume_usd']), this.locale, '', )} USD`;
-  public getAvailableSupply = (): string => `${formatCurrency(parseFloat(this.tickerData.available_supply), this.locale, '', )} BURST`;
+  public getPriceUsd = (): string => `${this.asCurrency(this.tickerData.price_usd, '1.0-6')} USD`;
+  public get24hVolume = (): string => `${this.asCurrency(this.tickerData['24h_volume_usd'])} USD`;
+  public getAvailableSupply = (): string => `${this.asCurrency(this.tickerData.available_supply)} BURST`;
   public getRelativeSupply = (): string => {
     const supply = parseFloat(this.tickerData.available_supply);
     const max_supply = parseFloat(this.tickerData.max_supply);
     const relative_supply = supply > 0 ? (supply / max_supply) : 0;
     return `${formatPercent(relative_supply, this.locale)}`;
   }
-  public getMarketCap = (): string => `${formatCurrency(parseFloat(this.tickerData.market_cap_usd), this.locale, '', )} USD`;
+  public getMarketCap = (): string => `${this.asCurrency(this.tickerData.market_cap_usd)} USD`;
   public getYesterDaysChange = (): string => `${parseFloat(this.tickerData.percent_change_24h).toFixed(2)} %`;
   public isChangeNegative = (): boolean => this.tickerData.percent_change_24h.startsWith('-');
   public getMarketRank = (): string => this.tickerData.rank;
