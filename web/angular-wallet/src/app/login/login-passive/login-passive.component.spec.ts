7ccc15a9e3910e7aef3add6ba0b1a5e9a9210311
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-05-12 06:01:09 -0700

    Fix/new account (#405)
    * fix(send): fix double encoding issue sending to new accounts
    
    * test(login): update tests
    
    * test(login): update tests
    
    * chore(i18n): minor lang tweak

--- web/angular-wallet/src/app/login/login-passive/login-passive.component.spec.ts
+++ web/angular-wallet/src/app/login/login-passive/login-passive.component.spec.ts
@@ -3,6 +3,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { LoginPassiveComponent } from './login-passive.component';
 import { MockComponent } from 'ng-mocks';
 import { CreatePassiveAccountComponent } from 'app/setup/account/create-passive/create-passive.component';
+import { BehaviorSubject } from 'rxjs';
+import { StoreService } from 'app/store/store.service';
+import { I18nService } from 'app/layout/components/i18n/i18n.service';
+import { I18nModule } from 'app/layout/components/i18n/i18n.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginPassiveComponent', () => {
   let component: LoginPassiveComponent;
@@ -11,11 +16,26 @@ describe('LoginPassiveComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule.withRoutes([])
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+        I18nModule
       ],
       declarations: [
         LoginPassiveComponent,
         MockComponent(CreatePassiveAccountComponent)
+      ],
+      providers: [
+        I18nService,
+        {
+          provide: StoreService,
+          useFactory: () => {
+            return {
+              ready: new BehaviorSubject(true),
+              getSettings: () => Promise.resolve({ language: 'en' }),
+              saveSettings: () => Promise.resolve(true)
+            };
+          }
+        }
       ]})
       .compileComponents();
   }));
