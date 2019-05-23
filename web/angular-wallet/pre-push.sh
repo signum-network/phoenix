b83bd10bcb6940a825d051b957249d896dcee5ef
ohager <oliver.hager@dextra-sw.com>
2019-05-23 08:38:19 -0300

    updated changelog and added fail check on pre-push script

--- web/angular-wallet/pre-push.sh
+++ web/angular-wallet/pre-push.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+
+check () {
+    if [ $? -ne 0 ]; then
+        echo "$1: $error"
+        exit 1
+    fi
+}
+
 cd ../../lib
 npm run tsc
 npm run test
+check
 cd ../web/angular-wallet
 npm run test
+check
 ng build --aot --source-map=false
