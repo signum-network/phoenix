ead4a8ab9ed7c35d058866a3c4a8f334a4e4c9c5
ohager <oliver.hager@dextra-sw.com>
2019-05-23 12:06:59 -0300

    layout improvements on pin generation

--- web/angular-wallet/src/app/setup/account/create-active/pin/pin.component.ts
+++ web/angular-wallet/src/app/setup/account/create-active/pin/pin.component.ts
@@ -1,43 +1,49 @@
 import {Component, Input} from '@angular/core';
-import { Router } from '@angular/router';
-import { CreateService } from '../../create.service';
-import { NotifierService } from 'angular-notifier';
+import {Router} from '@angular/router';
+import {CreateService} from '../../create.service';
+import {NotifierService} from 'angular-notifier';
 
 @Component({
-    selector: 'app-account-create-pin',
-    styleUrls: ['./pin.component.scss'],
-    templateUrl: './pin.component.html'
+  selector: 'app-account-create-pin',
+  styleUrls: ['./pin.component.scss'],
+  templateUrl: './pin.component.html'
 })
 export class AccountCreatePinComponent {
 
-  @Input('pin') pin: string;
+  @Input() pin: string;
 
   constructor(
-        private router: Router,
-        private createService: CreateService,
-        private notificationService: NotifierService,
-    ) { }
-    public back(): void {
-        this.createService.setStepIndex(1);
-    }
+    private router: Router,
+    private createService: CreateService,
+    private notificationService: NotifierService,
+  ) {
+  }
 
-    public finish(pin: string): void {
-        if (!pin || !pin.length) {
-            return;
-        }
-        this.createService.setPin(pin);
-        this.createService.createActiveAccount().then((success) => {
-            this.notificationService.notify('success', `Account added successfully`);
-            this.createService.reset();
-            // Angular Stepper hack
-            setTimeout(x => {
-                this.createService.setStepIndex(0);
-            }, 0);
-            this.router.navigate(['/']);
-          },
-          (error) => {
-            this.notificationService.notify('error', error.toString());
-          });
+  public back(): void {
+    this.createService.setStepIndex(1);
+  }
+
+  public get isValid(): boolean {
+    return this.pin && this.pin.length >= 4;
+  }
+
+  public finish(): void {
+    if (!this.isValid) {
+      return;
     }
+    this.createService.setPin(this.pin);
+    this.createService.createActiveAccount().then((success) => {
+        this.notificationService.notify('success', `Account added successfully`);
+        this.createService.reset();
+        // Angular Stepper hack
+        setTimeout(x => {
+          this.createService.setStepIndex(0);
+        }, 0);
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.notificationService.notify('error', error.toString());
+      });
+  }
 
 }
