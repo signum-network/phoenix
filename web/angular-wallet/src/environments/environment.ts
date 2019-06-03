5f3d8c3154cb68f7769f0833800365ed09157a10
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-06-02 18:30:24 -0700

    feat(web): add npm run build:web for web nodes

--- web/angular-wallet/src/environments/environment.ts
+++ web/angular-wallet/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   version,
   defaultNode: 'http://localhost:4200',
   market: {
-    tickerUrl: '/v1/ticker/burst/',
+    tickerUrl: 'https://api.coinmarketcap.com/v1/ticker/burst/',
     tickerInterval: 30 * 1000
   }
 };
