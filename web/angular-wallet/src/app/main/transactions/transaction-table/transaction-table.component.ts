d44ee396aae2d097b2596954b45ea6feacb6d1d8
ohager <oliver.hager@dextra-sw.com>
2019-06-09 20:40:05 -0300

    fixed #523 - tx amounts in table on dashboard colored correctly

--- web/angular-wallet/src/app/main/transactions/transaction-table/transaction-table.component.ts
+++ web/angular-wallet/src/app/main/transactions/transaction-table/transaction-table.component.ts
@@ -1,41 +1,33 @@
 import {
   Component,
   Input,
-  OnInit,
   ViewChild,
-  ChangeDetectionStrategy,
-  AfterViewInit,
-  OnChanges,
-  SimpleChanges
-} from '@angular/core';
+  AfterViewInit} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import {ActivatedRoute} from '@angular/router';
 import {
   Transaction,
   Account,
   isMultiOutSameTransaction,
-  isMultiOutTransaction, getRecipientsAmount
+  isMultiOutTransaction,
+  getRecipientsAmount
 } from '@burstjs/core';
 import {convertBurstTimeToDate, convertNQTStringToNumber} from '@burstjs/util';
 import {UtilService} from 'app/util.service';
 import {takeUntil} from 'rxjs/operators';
 import {UnsubscribeOnDestroy} from '../../../util/UnsubscribeOnDestroy';
 import {StoreService} from '../../../store/store.service';
-import {AccountService} from 'app/setup/account/account.service';
 
 @Component({
   selector: 'app-transaction-table',
   styleUrls: ['./transaction-table.component.scss'],
   templateUrl: './transaction-table.component.html',
 })
-export class TransactionTableComponent extends UnsubscribeOnDestroy implements OnInit, AfterViewInit, OnChanges {
+export class TransactionTableComponent extends UnsubscribeOnDestroy implements AfterViewInit {
   public locale: string;
 
   constructor(private utilService: UtilService,
-              private storeService: StoreService,
-              private route: ActivatedRoute,
-              private accountService: AccountService) {
+              private storeService: StoreService) {
     super();
     this.storeService.settings
       .pipe(
@@ -46,44 +38,18 @@ export class TransactionTableComponent extends UnsubscribeOnDestroy implements O
       });
   }
 
-
   public convertNQTStringToNumber = convertNQTStringToNumber;
-  private account: Account;
 
   @Input() dataSource: MatTableDataSource<Transaction>;
   @Input() public displayedColumns = ['transaction_id', 'attachment', 'timestamp', 'type', 'amount', 'fee', 'account', 'confirmations'];
   @Input() paginationEnabled = true;
+  @Input() account: Account;
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
   public isMultiOutPayment(transaction: Transaction): boolean {
     return isMultiOutSameTransaction(transaction) || isMultiOutTransaction(transaction);
   }
 
-  public ngOnInit(): void {
-
-    const referencedAccount = this.route.snapshot.data.account;
-    if (referencedAccount) {
-      this.setAccount(referencedAccount);
-      return;
-    }
-
-    this.accountService.currentAccount
-      .pipe(
-        takeUntil(this.unsubscribeAll)
-      )
-      .subscribe((account) => {
-        this.setAccount(account);
-      });
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const referencedAccount = this.route.snapshot.data.account;
-    this.setAccount(referencedAccount);
-  }
-
-  public setAccount(account: Account): void {
-    this.account = account;
-  }
 
   public ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
