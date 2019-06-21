ccc6c2dd32cb79c9610f9b3ee6a3ce348ed33b6d
ohager <oliver.hager@dextra-sw.com>
2019-06-20 23:04:51 -0300

    fix send multiout same (#545)
    * fixed #543
    
    * updated changelog

--- web/angular-wallet/src/app/main/send-burst/send-multi-out-form/send-multi-out-form.component.spec.ts
+++ web/angular-wallet/src/app/main/send-burst/send-multi-out-form/send-multi-out-form.component.spec.ts
@@ -1,17 +1,17 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {NetworkModule} from 'app/network/network.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {I18nModule} from 'app/layout/components/i18n/i18n.module';
 import {NgxMaskModule} from 'ngx-mask';
 import {NotifierModule} from 'angular-notifier';
@@ -35,90 +35,118 @@ import {Account} from '@burstjs/core';
 import {WarnSendDialogComponent} from '../warn-send-dialog/warn-send-dialog.component';
 
 describe('SendMultiOutFormComponent', () => {
-  let component: SendMultiOutFormComponent;
-  let fixture: ComponentFixture<SendMultiOutFormComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        NotifierModule,
-        NgxMaskModule.forRoot(),
-        I18nModule,
-        MatInputModule,
-        MatButtonModule,
-        MatIconModule,
-        MatFormFieldModule,
-        MatCardModule,
-        MatSelectModule,
-        Ng5SliderModule,
-        MatGridListModule,
-        MatCheckboxModule,
-        FormsModule,
-        NetworkModule,
-        HttpClientTestingModule,
-        MatProgressSpinnerModule,
-        MatTooltipModule,
-        MatDialogModule
-      ],
-      declarations: [
-        SendMultiOutFormComponent,
-        BurstInputValidatorDirective,
-        BurstFeeSelectorComponent,
-        BurstRecipientInputComponent,
-        WarnSendDialogComponent
-      ],
-      providers: [
-        I18nService,
-        {
-          provide: StoreService,
-          useFactory: () => {
-            return {
-              ready: new BehaviorSubject(true),
-              settings: new BehaviorSubject(true)
-            }
-          }
-        },
-        {
-          provide: TransactionService,
-          useFactory: () => {
-            return {
-              sendMoney: () => Promise.resolve({broadcasted: true})
-            }
-          }
-        },
-        {
-          provide: AccountService,
-          useFactory: () => {
-            return {
-              currentAccount: new BehaviorSubject(true)
-            }
-          }
-        },
-        BurstInputValidatorDirective
-      ]
-    })
-      .compileComponents();
-  }));
+  describe('Render Testing', () => {
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SendMultiOutFormComponent);
-    component = fixture.componentInstance;
-    component.fees = {
-      minimum: 123,
-      standard: 123,
-      cheap: 123,
-      priority: 123,
-      requestProcessingTime: 1
-    }
-    component.account = new Account({
-      account: '123'
-    })
-    fixture.detectChanges();
+
+    let component: SendMultiOutFormComponent;
+    let fixture: ComponentFixture<SendMultiOutFormComponent>;
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          BrowserModule,
+          BrowserAnimationsModule,
+          NotifierModule,
+          NgxMaskModule.forRoot(),
+          I18nModule,
+          MatInputModule,
+          MatButtonModule,
+          MatIconModule,
+          MatFormFieldModule,
+          MatCardModule,
+          MatSelectModule,
+          Ng5SliderModule,
+          MatGridListModule,
+          MatCheckboxModule,
+          FormsModule,
+          NetworkModule,
+          HttpClientTestingModule,
+          MatProgressSpinnerModule,
+          MatTooltipModule,
+          MatDialogModule
+        ],
+        declarations: [
+          SendMultiOutFormComponent,
+          BurstInputValidatorDirective,
+          BurstFeeSelectorComponent,
+          BurstRecipientInputComponent,
+          WarnSendDialogComponent
+        ],
+        providers: [
+          I18nService,
+          {
+            provide: StoreService,
+            useFactory: () => {
+              return {
+                ready: new BehaviorSubject(true),
+                settings: new BehaviorSubject(true)
+              }
+            }
+          },
+          {
+            provide: TransactionService,
+            useFactory: () => {
+              return {
+                sendMoney: () => Promise.resolve({broadcasted: true})
+              }
+            }
+          },
+          {
+            provide: AccountService,
+            useFactory: () => {
+              return {
+                currentAccount: new BehaviorSubject(true)
+              }
+            }
+          },
+          BurstInputValidatorDirective
+        ]
+      })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SendMultiOutFormComponent);
+      component = fixture.componentInstance;
+      component.fees = {
+        minimum: 123,
+        standard: 123,
+        cheap: 123,
+        priority: 123,
+        requestProcessingTime: 1
+      };
+      component.account = new Account({
+        account: '123'
+      });
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Inner Functions', () => {
+    const storeServiceMock =  {
+      ready: new BehaviorSubject(true),
+      settings: new BehaviorSubject(true)
+    };
+
+    const component = new SendMultiOutFormComponent(
+      null,
+      null,
+      null,
+      null,
+      storeServiceMock);
+
+    it('should calculate same amount correctly', () => {
+      component.sameAmount = true;
+      // @ts-ignore
+      component.recipients = [{addressRaw: '1'}, {addressRaw: '2'}];
+      component.amountNQT = '1000';
+      component.feeNQT = '10';
+      expect(component.getTotal()).toBe(2010);
+    });
   });
 });
