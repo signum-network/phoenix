e7dbb7732e639161085c7557c04c1b06db1beff4
ohager <oliver.hager@dextra-sw.com>
2019-06-26 00:44:54 -0300

    fixed build problem

--- web/angular-wallet/src/app/main/transactions/transaction.service.ts
+++ web/angular-wallet/src/app/main/transactions/transaction.service.ts
@@ -1,78 +1,82 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { TransactionApi, TransactionId, EncryptedMessage, Message } from '@burstjs/core';
-import { Keys, decryptAES, hashSHA256, encryptMessage } from '@burstjs/crypto';
-import { ApiService } from '../../api.service';
-import { AccountService } from 'app/setup/account/account.service';
-import { convertAddressToNumericId } from '@burstjs/util/out';
-import { StoreService } from 'app/store/store.service';
-import { Settings } from 'app/settings';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {TransactionApi, TransactionId, EncryptedMessage, Message} from '@burstjs/core';
+import {Keys, decryptAES, hashSHA256, encryptMessage} from '@burstjs/crypto';
+import {ApiService} from '../../api.service';
+import {AccountService} from 'app/setup/account/account.service';
+import {convertAddressToNumericId} from '@burstjs/util/out';
+import {StoreService} from 'app/store/store.service';
+import {Settings} from 'app/settings';
 
 interface SendMoneyRequest {
-    transaction: {
-        amountNQT: string;
-        feeNQT: string;
-        attachment: EncryptedMessage | Message;
-        deadline: number;
-        fullHash: string;
-        type: number;
-    };
-    pin: string;
-    keys: Keys;
-    recipientAddress: string;
+  transaction: {
+    amountNQT: string;
+    feeNQT: string;
+    attachment: EncryptedMessage | Message;
+    deadline: number;
+    fullHash: string;
+    type: number;
+  };
+  pin: string;
+  keys: Keys;
+  recipientAddress: string;
 }
 
 
 interface SendMoneyMultiOutRequest {
-    transaction: {
-        feeNQT: string;
-        recipients: string;
-        deadline: number;
-        amountNQT?: string;
-        fullHash?: string;
-    };
-    pin: string;
-    sameAmount: boolean;
-    keys: Keys;
+  transaction: {
+    feeNQT: string;
+    recipients: string;
+    deadline: number;
+    amountNQT?: string;
+    fullHash?: string;
+  };
+  pin: string;
+  sameAmount: boolean;
+  keys: Keys;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class TransactionService {
-    private transactionApi: TransactionApi;
+  private transactionApi: TransactionApi;
 
-    public currentAccount: BehaviorSubject<any> = new BehaviorSubject(undefined);
+  public currentAccount: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
-    constructor(apiService: ApiService, private accountService: AccountService, private storeService: StoreService) {
-        this.transactionApi = apiService.api.transaction;
-        this.storeService.settings.subscribe((settings: Settings) => {
-            this.transactionApi = apiService.api.transaction;
-        });
+  constructor(apiService: ApiService, private accountService: AccountService, private storeService: StoreService) {
+    this.transactionApi = apiService.api.transaction;
+    this.storeService.settings.subscribe((settings: Settings) => {
+      this.transactionApi = apiService.api.transaction;
+    });
+  }
+
+  public getTransaction(id: string) {
+    return this.transactionApi.getTransaction(id);
+  }
+
+  public async sendMoney({transaction, pin, keys, recipientAddress}: SendMoneyRequest) {
+    if (transaction.attachment && (<EncryptedMessage>transaction.attachment).data) {
+
+      const recipientAccountId = convertAddressToNumericId(recipientAddress);
+      const recipient = await this.accountService.getAccount(recipientAccountId);
+      const agreementPrivateKey = decryptAES(keys.agreementPrivateKey, hashSHA256(pin));
+      const encryptedMessage = encryptMessage(
+        (<EncryptedMessage>transaction.attachment).data,
+        // @ts-ignore
+        recipient.publicKey,
+        agreementPrivateKey
+      );
+      // @ts-ignore
+      transaction.attachment = encryptedMessage;
+      transaction.attachment.type = 'encrypted_message';
     }
+    const senderPrivateKey = decryptAES(keys.signPrivateKey, hashSHA256(pin));
+    return this.transactionApi.sendMoney(transaction, keys.publicKey, senderPrivateKey, recipientAddress);
+  }
 
-    public getTransaction(id: string) {
-        return this.transactionApi.getTransaction(id);
-    }
-
-    public async sendMoney({ transaction, pin, keys, recipientAddress }: SendMoneyRequest) {
-        if (transaction.attachment && (<EncryptedMessage>transaction.attachment).data) {
-
-            const recipientAccountId = await convertAddressToNumericId(recipientAddress);
-            const recipient = await this.accountService.getAccount(recipientAccountId);
-            const agreementPrivateKey = decryptAES(keys.agreementPrivateKey, hashSHA256(pin));
-            const encryptedMessage = await encryptMessage((<EncryptedMessage>transaction.attachment).data,
-            // @ts-ignore
-            recipient.publicKey,
-            agreementPrivateKey);
-            transaction.attachment =  encryptedMessage;
-            transaction.attachment.type = 'encrypted_message';
-        }
-        const senderPrivateKey = decryptAES(keys.signPrivateKey, hashSHA256(pin));
-        return this.transactionApi.sendMoney(transaction, keys.publicKey, senderPrivateKey, recipientAddress);
-    }
-    public async sendMoneyMultiOut({ transaction, pin, keys, sameAmount }: SendMoneyMultiOutRequest) : Promise<TransactionId> {
-        const senderPrivateKey = decryptAES(keys.signPrivateKey, hashSHA256(pin));
-        return this.transactionApi.sendMoneyMultiOut(transaction, keys.publicKey, senderPrivateKey, transaction.recipients, sameAmount);
-    }
+  public async sendMoneyMultiOut({transaction, pin, keys, sameAmount}: SendMoneyMultiOutRequest): Promise<TransactionId> {
+    const senderPrivateKey = decryptAES(keys.signPrivateKey, hashSHA256(pin));
+    return this.transactionApi.sendMoneyMultiOut(transaction, keys.publicKey, senderPrivateKey, transaction.recipients, sameAmount);
+  }
 }
