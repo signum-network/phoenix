9cc3900c9831c6d111e03e5584a6a6b6b2a596b5
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-07-21 12:10:14 -0700

    lint fixes

--- mobile/src/modules/cmc/store/actions.ts
+++ mobile/src/modules/cmc/store/actions.ts
@@ -8,10 +8,8 @@ const actions = {
 
 export const loadCMCData = createActionFn<void, Promise<void>>(
   async (dispatch, _getState) => {
-    console.log('in fetch price info');
     const response = await fetch(defaultSettings.coinMarketCapURL);
     const updatedPriceInfo = await response.json();
-    console.log(updatedPriceInfo);
     if (updatedPriceInfo.length) {
       dispatch(actions.updatePriceInfo(updatedPriceInfo[0]));
     }
