842d576569747e50bdbff3ec00131d7022995688
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2019-09-22 12:55:11 -0700

    chore: fix lint

--- mobile/src/modules/transactions/store/utils.ts
+++ mobile/src/modules/transactions/store/utils.ts
@@ -2,10 +2,11 @@ export function parseURLParams(url: string): any {
   const regex = /[?&]([^=#]+)=([^&#]*)/g;
   const params = {};
   let match;
+  // @ts-ignore
   // tslint:disable-next-line: no-conditional-assignment
   while (match = regex.exec(url)) {
 
-  // @ts-ignore
+    // @ts-ignore
     params[match[1]] = match[2];
   }
   return params;
