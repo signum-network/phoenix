1a2a1d6e58762a5e41902b1a96b0fbe56431d558
ohager <oliver.hager@dextra-sw.com>
2019-11-07 21:10:14 -0300

    Added configurations to http

--- lib/packages/http/src/__tests__/e2e/http.spec.e2e.ts
+++ lib/packages/http/src/__tests__/e2e/http.spec.e2e.ts
@@ -9,6 +9,34 @@ describe('[E2E] Http', () => {
         expect(collaborators.response.length).toBeGreaterThan(1);
     });
 
+    it('should get contributors with some additional _global_ options from this github repo', async () => {
+
+        const customOptions = {
+            validateStatus: jest.fn((status) => true)
+        };
+
+        const github = new Http('https://api.github.com', customOptions);
+        const collaborators = await github.get('/repos/burst-apps-team/phoenix/contributors');
+        expect(collaborators.status).toBe(200);
+        expect(collaborators.response.length).toBeGreaterThan(1);
+        expect(customOptions.validateStatus).toHaveBeenCalledTimes(1);
+        expect(customOptions.validateStatus).toHaveBeenCalledWith(200);
+    });
+
+    it('should get contributors with some additional options from this github repo', async () => {
+
+        const customOptions = {
+            validateStatus: jest.fn((status) => true)
+        };
+
+        const github = new Http('https://api.github.com');
+        const collaborators = await github.get('/repos/burst-apps-team/phoenix/contributors', customOptions);
+        expect(collaborators.status).toBe(200);
+        expect(collaborators.response.length).toBeGreaterThan(1);
+        expect(customOptions.validateStatus).toHaveBeenCalledTimes(1);
+        expect(customOptions.validateStatus).toHaveBeenCalledWith(200);
+    });
+
     it('should get not found error from this github repo', async () => {
         try {
             const github = new Http('https://api.github.com');
--- lib/packages/http/src/httpImpl.ts
+++ lib/packages/http/src/httpImpl.ts
@@ -5,6 +5,8 @@ import HttpResponse from './httpResponse';
 import HttpError from './httpError';
 import Http from './http';
 
+const DefaultAxiosOptions = {};
+
 /**
  * Generic Http client
  */
@@ -15,11 +17,15 @@ export default class HttpImpl implements Http {
     /**
      * Creates your Http client
      * @param baseURL The baseUrl, i.e host url
+     * @param options [optional] An options/configurations object applied to all requests
+     * The current implementation uses axios, so the options can be found here
+     * [Axios Configuration](https://github.com/axios/axios#request-config)
      */
-    constructor(baseURL: string) {
+    constructor(baseURL: string, options: any = DefaultAxiosOptions) {
         // see https://github.com/axios/axios#request-config for more
         this._clientImpl = axios.create({
             baseURL,
+            ...options
         });
     }
 
@@ -32,36 +38,36 @@ export default class HttpImpl implements Http {
         return new HttpError(url, -1, 'Http Configuration error', null);
     }
 
-    async get(url: string): Promise<HttpResponse> {
+    async get(url: string, options?: any): Promise<HttpResponse> {
         try {
-            const {status, data} = await this._clientImpl.get(url);
+            const {status, data} = await this._clientImpl.get(url, options);
             return new HttpResponse(status, data);
         } catch (error) {
             throw HttpImpl.mountError(url, error);
         }
     }
 
-    async post(url: string, payload: any): Promise<HttpResponse> {
+    async post(url: string, payload: any, options?: any): Promise<HttpResponse> {
         try {
-            const {status, data} = await this._clientImpl.post(url, payload);
+            const {status, data} = await this._clientImpl.post(url, payload, options);
             return new HttpResponse(status, data);
         } catch (error) {
             throw HttpImpl.mountError(url, error);
         }
     }
 
-    async put(url: string, payload: any): Promise<HttpResponse> {
+    async put(url: string, payload: any, options?: any): Promise<HttpResponse> {
         try {
-            const {status, data} = await this._clientImpl.put(url, payload);
+            const {status, data} = await this._clientImpl.put(url, payload, options);
             return new HttpResponse(status, data);
         } catch (error) {
             throw HttpImpl.mountError(url, error);
         }
     }
 
-    async delete(url: string): Promise<HttpResponse> {
+    async delete(url: string, options?: any): Promise<HttpResponse> {
         try {
-            const {status, data} = await this._clientImpl.delete(url);
+            const {status, data} = await this._clientImpl.delete(url, options);
             return new HttpResponse(status, data);
         } catch (error) {
             throw HttpImpl.mountError(url, error);
