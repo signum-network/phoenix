590c592e0e82ad39e8f12f0e823a72bcfb3b3fe9
ohager <oliver.hager@dextra-sw.com>
2019-11-16 17:19:32 -0300

    adjusted web wallet for new args

--- web/angular-wallet/src/app/setup/account/account.service.ts
+++ web/angular-wallet/src/app/setup/account/account.service.ts
@@ -85,22 +85,33 @@ export class AccountService {
   }
 
   public async getAccountTransactions(
-    id: string,
+    accountId: string,
     firstIndex?: number,
     lastIndex?: number,
     numberOfConfirmations?: number,
     type?: number,
     subtype?: number
   ): Promise<TransactionList> {
+
+    const args = {
+      accountId,
+      firstIndex,
+      lastIndex,
+      numberOfConfirmations,
+      type,
+      subtype,
+    };
     try {
       const includeMultiouts = semver.gte(this.selectedNode.version, constants.multiOutMinVersion, {includePrerelease: true}) || undefined;
-      const transactions = await this.api.account.getAccountTransactions(
-        id, firstIndex, lastIndex, numberOfConfirmations, type, subtype, includeMultiouts);
+      const transactions = await this.api.account.getAccountTransactions({
+        ...args,
+        includeIndirect: includeMultiouts
+      });
       return Promise.resolve(transactions);
     } catch (e) {
       const EC_INVALID_ARG = 4;
       if (e.data.errorCode === EC_INVALID_ARG) {
-        return this.api.account.getAccountTransactions(id, firstIndex, lastIndex, numberOfConfirmations, type, subtype);
+        return await this.api.account.getAccountTransactions(args);
       } else {
         throw e;
       }
