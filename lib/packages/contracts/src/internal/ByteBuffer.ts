8417b3f20deef8e71537bca1a4773177c4d1210a
ohager <oliver.hager@dextra-sw.com>
2019-12-10 21:10:19 -0300

    test: Added unit tests for ButeBuffer

--- lib/packages/contracts/src/internal/ByteBuffer.ts
+++ lib/packages/contracts/src/internal/ByteBuffer.ts
@@ -4,8 +4,19 @@
 import BigNumber from 'bignumber.js';
 import './padStartPolyfill';
 
+const MaxUInt64 = new BigNumber('18446744073709551615');
+
+function assertRange(value, min, max) {
+    if (min <= value && value <= max) {
+        return;
+    }
+    throw Error(`Out of range (${min}, ${max}): ${value}`);
+}
+
 /**
  * Helper class to deal easier with bytes
+ *
+ * The implementations are not performant
  */
 export class ByteBuffer {
     private view: DataView;
@@ -16,22 +27,29 @@ export class ByteBuffer {
     }
 
     putByte(byte: number) {
+        assertRange(byte, -128, 255);
         this.view.setInt8(this.offset, byte);
         this.offset += 1;
     }
 
     putShort(int16: number) {
+        assertRange(int16, -(2 ** 15) - 1, 2 ** 16 - 1);
         this.view.setInt16(this.offset, int16, this.littleEndian);
         this.offset += 2;
     }
 
     putUInt32(uint32: number) {
+        assertRange(uint32, 0, 2 ** 32 - 1);
         this.view.setUint32(this.offset, uint32, this.littleEndian);
         this.offset += 4;
     }
 
     putUInt64(value: number) {
         const bigNumber = new BigNumber(value);
+
+        if (value < 0 || bigNumber.gt(MaxUInt64)) {
+            throw Error(`Out of range (${0}, ${MaxUInt64})`);
+        }
         // @ts-ignore
         const hex = bigNumber.toString(16).padStart(16, '0');
         const upperUint32 = parseInt(hex.slice(0, 8), 16);
@@ -45,7 +63,7 @@ export class ByteBuffer {
         }
     }
 
-    putBytes(bytes: Int8Array) {
+    putBytes(bytes: number[]) {
         // not a fast implementation!
         const l = bytes.length;
         if (this.littleEndian) {
--- lib/packages/contracts/src/internal/__tests__/ByteBuffer.spec.ts
+++ lib/packages/contracts/src/internal/__tests__/ByteBuffer.spec.ts
@@ -2,6 +2,115 @@ import {ByteBuffer} from '../ByteBuffer';
 
 describe('ByteBuffer', () => {
 
+    describe('putByte', () => {
+        it('puts a single byte - Big Endian', () => {
+            const byteBuffer = new ByteBuffer(2, false);
+            byteBuffer.putByte(1);
+            byteBuffer.putByte(2);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([1, 2]));
+        });
+
+        it('puts a single signed byte - Little Endian', () => {
+            const byteBuffer = new ByteBuffer(2, true);
+            byteBuffer.putByte(1);
+            byteBuffer.putByte(-2);
+            const bytes = new Int8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Int8Array([1, -2]));
+        });
+
+        it('puts a single byte - Buffer overflow', () => {
+            const byteBuffer = new ByteBuffer(1, false);
+            byteBuffer.putByte(1);
+            try {
+                byteBuffer.putByte(2);
+                expect('Expects exception').toBeFalsy();
+            } catch (e) {
+                expect(e.message).toContain('Offset is outside the bounds');
+            }
+        });
+
+        it('puts a single byte - Out of range', () => {
+            const byteBuffer = new ByteBuffer(1, false);
+            try {
+                byteBuffer.putByte(345);
+                expect('Expects exception').toBeFalsy();
+            } catch (e) {
+                expect(e.message).toContain('Out of range');
+            }
+        });
+    });
+
+    describe('putBytes', () => {
+        it('puts multiple bytes - Big Endian', () => {
+            const byteBuffer = new ByteBuffer(4, false);
+            byteBuffer.putBytes([-1, 0, 1, 2]);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([255, 0, 1, 2]));
+        });
+
+        it('puts a multiple bytes - Little Endian', () => {
+            const byteBuffer = new ByteBuffer(4, true);
+            byteBuffer.putBytes([-1, 0, 1, 2]);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([2, 1, 0, 255]));
+        });
+    });
+
+    describe('putShort', () => {
+        it('puts short - Big Endian', () => {
+            const byteBuffer = new ByteBuffer(2, false);
+            byteBuffer.putShort(12345);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([48, 57]));
+        });
+
+        it('puts a short - Little Endian', () => {
+            const byteBuffer = new ByteBuffer(2, true);
+            byteBuffer.putShort(12345);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([57,48]));
+        });
+
+        it('puts a short - Out of range', () => {
+            const byteBuffer = new ByteBuffer(2, false);
+            try {
+                byteBuffer.putShort(400000);
+                expect('Expects exception').toBeFalsy();
+            } catch (e) {
+                expect(e.message).toContain('Out of range');
+            }
+        });
+
+    });
+
+    describe('putUint32', () => {
+        it('puts an Uint32- Big Endian', () => {
+            const byteBuffer = new ByteBuffer(4, false);
+            byteBuffer.putUInt32(12345678);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([0, 188, 97, 78]));
+        });
+
+        it('puts an Uint32 - Little Endian', () => {
+            const byteBuffer = new ByteBuffer(4, true);
+            byteBuffer.putUInt32(12345678);
+            const bytes = new Uint8Array(byteBuffer.getBytes());
+            expect(bytes).toEqual(new Uint8Array([78, 97, 188, 0]));
+        });
+
+        it('puts an Uint32 - Out of range', () => {
+            const byteBuffer = new ByteBuffer(4, false);
+            try {
+                byteBuffer.putShort(400000);
+                expect('Expects exception').toBeFalsy();
+            } catch (e) {
+                expect(e.message).toContain('Out of range');
+            }
+        });
+
+    });
+
     describe('putUInt64', () => {
         it('puts long number correctly into byte buffer - Big Endian', () => {
             const long = 12345689123456789;
