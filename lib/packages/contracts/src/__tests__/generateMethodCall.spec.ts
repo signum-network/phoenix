42a3272b4919fd788d580bd6ceedf037b37bfd3a
ohager <oliver.hager@dextra-sw.com>
2019-12-13 23:09:30 -0300

    chore: Own review cleanup

--- lib/packages/contracts/src/__tests__/generateMethodCall.spec.ts
+++ lib/packages/contracts/src/__tests__/generateMethodCall.spec.ts
@@ -17,12 +17,4 @@ describe('generateMethodCall', () => {
         });
         expect(message).toBe('17f898a0f498d0900100000000000000d204000000000000d202964900000000');
     });
-
-    it('generates a method call with three arguments', () => {
-        const message = generateMethodCall({
-            methodHash: ContractMethodHash,
-            methodArgs: [true, 1234, '1234567890'],
-        });
-        expect(message).toBe('17f898a0f498d0900100000000000000d204000000000000d202964900000000');
-    });
 });
