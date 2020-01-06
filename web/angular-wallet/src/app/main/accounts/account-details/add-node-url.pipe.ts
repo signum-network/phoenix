b375e1a4cc52827cf6ea7744d46290a43c700d3a
blankey1337 <42594751+blankey1337@users.noreply.github.com>
2020-01-05 18:09:28 -0800

    more cleanup

--- web/angular-wallet/src/app/main/accounts/account-details/add-node-url.pipe.ts
+++ web/angular-wallet/src/app/main/accounts/account-details/add-node-url.pipe.ts
@@ -1,19 +1,19 @@
-import { Pipe, PipeTransform } from "@angular/core";
-import { ApiService } from "app/api.service";
+import { Pipe, PipeTransform } from '@angular/core';
+import { ApiService } from 'app/api.service';
 
 @Pipe({ name: 'addNodeUrl' })
 export class AddNodeUrlPipe implements PipeTransform {
 
-    nodeUrl: string = '';
-    constructor(private apiService: ApiService) {
-        this.nodeUrl = this.apiService.nodeUrl;
-    }
+  nodeUrl = '';
+  constructor(private apiService: ApiService) {
+    this.nodeUrl = this.apiService.nodeUrl;
+  }
 
-    transform(value: string): string {
-        if (value && value.startsWith('/')) {
-            // remove "/" to avoid double slashes
-            value = value.substr(1, value.length);
-        }
-        return value && `${this.nodeUrl}/${value}`;
+  transform(value: string): string {
+    if (value && value.startsWith('/')) {
+      // remove "/" to avoid double slashes
+      value = value.substr(1, value.length);
     }
+    return value && `${this.nodeUrl}/${value}`;
+  }
 }
