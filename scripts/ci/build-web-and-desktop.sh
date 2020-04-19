f60b367d102c60a212cbaa71a363f815ca831c46
ohager <oliver.hager@dextra-sw.com>
2020-04-18 23:20:57 -0300

    ci: still struggling...retry globbing

--- scripts/ci/build-web-and-desktop.sh
+++ scripts/ci/build-web-and-desktop.sh
@@ -9,14 +9,14 @@ source ./scripts/ci/get-version.sh
 ## install script deps
 cd scripts
 npm install
-#
+
 echo ----------------------------------
 echo "Building BurstJS"
 echo ----------------------------------
 cd ../lib
 npm install
 npm run bootstrap
-#
+
 ## install angular deps
 cd ../web/angular-wallet
 npm install
@@ -32,12 +32,15 @@ echo ----------------------------------
 echo "Building Web Version"
 echo ----------------------------------
 cd ../../web/angular-wallet
-TARGET_DIR=../../../desktop/wallet/release-builds/
-WEB_ARCHIVE="../web-phoenix-burst-wallet.${PACKAGE_VERSION}.tar.gz"
+WEB_ARCHIVE="web-phoenix-burst-wallet.${PACKAGE_VERSION}.tar.gz"
 npm run build:web
 echo "Archiving..."
 cd dist
 tar cfz ${WEB_ARCHIVE} *
 echo "Moving..."
-mv ${WEB_ARCHIVE} ${TARGET_DIR}
-
+cd ../../../
+mkdir deploy
+mv ./desktop/wallet/release-builds/linux-phoenix* ./deploy
+mv ./desktop/wallet/release-builds/mac-phoenix* ./deploy
+mv ./desktop/wallet/release-builds/win-phoenix* ./deploy
+mv "./web/angular-wallet/dist/${WEB_ARCHIVE}" ./deploy
