1e61a3f1608186ac8615dd7f4a3ec7e4d510a3d6
ohager <oliver.hager@dextra-sw.com>
2020-04-18 21:11:34 -0300

    ci: I think I got it - deploying web as asset

---
+++ scripts/ci/build-web-and-desktop.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+echo
+echo ==================================
+echo "Building Wallet for Desktop & Web"
+echo ==================================
+echo
+source ./scripts/ci/get-version.sh
+
+## install script deps
+cd scripts
+#npm install
+#
+echo ----------------------------------
+echo "Building BurstJS"
+echo ----------------------------------
+cd ../lib
+#npm install
+#npm run bootstrap
+#
+## install angular deps
+cd ../web/angular-wallet
+#npm install
+
+echo ----------------------------------
+echo "Building Desktop Version"
+echo ----------------------------------
+cd ../../desktop/wallet
+npm install
+npm run pack
+#npm run release:all
+
+echo ----------------------------------
+echo "Building Web Version"
+echo ----------------------------------
+cd ../../web/angular-wallet
+TARGET_DIR=../../../desktop/wallet/release-builds/
+WEB_ARCHIVE="../web-phoenix-burst-wallet.${PACKAGE_VERSION}.tar.gz"
+npm run build:web
+echo "Archiving..."
+cd dist
+tar cfz ${WEB_ARCHIVE} *
+echo "Moving..."
+mv ${WEB_ARCHIVE} ${TARGET_DIR}
+
