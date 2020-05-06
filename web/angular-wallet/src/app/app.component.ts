45c704d4eb0b744d27e37b461c0a002027876f83
ohager <oliver.hager@dextra-sw.com>
2020-05-05 21:37:48 -0300

    fix: set new version flag to false

--- web/angular-wallet/src/app/app.component.ts
+++ web/angular-wallet/src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent extends UnsubscribeOnDestroy implements OnInit, OnDest
   firstTime = true;
   isScanning = false;
   isDownloadingUpdate = false;
-  newVersionAvailable = true;
+  newVersionAvailable = false;
   updateInfo: UpdateInfo;
   downloadingBlockchain = false;
   numberOfBlocks: number;
