1b97776f31525b14cfc24a1c84c48c5c16f1c8d3
ohager <oliver.hager@dextra-sw.com>
2020-06-24 12:07:59 -0300

    test: adjusted generate method call unit test using a real world hash

--- lib/packages/contracts/src/__tests__/generateMethodCall.spec.ts
+++ lib/packages/contracts/src/__tests__/generateMethodCall.spec.ts
@@ -1,20 +1,20 @@
 import {generateMethodCall} from '../generateMethodCall';
 
-const ContractMethodHash = '-8011735560658290665';
+const ContractMethodHash = '-327803124352370';
 
 describe('generateMethodCall', () => {
     it('generates a method call without argument', () => {
         const message = generateMethodCall({
             methodHash: ContractMethodHash
         });
-        expect(message).toBe('17f898a0f498d090');
+        expect(message).toBe('8e7a3763ddd5feff');
     });
 
     it('generates a method call with three arguments', () => {
         const message = generateMethodCall({
             methodHash: ContractMethodHash,
-            methodArgs: [true, 1234, '1234567890'],
+            methodArgs: [true, 1234, '-1234567890'],
         });
-        expect(message).toBe('17f898a0f498d0900100000000000000d204000000000000d202964900000000');
+        expect(message).toBe('8e7a3763ddd5feff0100000000000000d2040000000000002efd69b6ffffffff');
     });
 });
