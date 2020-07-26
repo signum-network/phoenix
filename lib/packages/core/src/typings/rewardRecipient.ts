e87052609ff282c9e00666f435cf0f4567535997
ohager <oliver@devbutze.com>
2020-07-26 17:16:35 -0300

    Release/burstjs/0.5.beta.12 (#1356)
    * chore: doc
    
    * chore: updating docs
    
    * v0.5.0-beta.12

--- lib/packages/core/src/typings/rewardRecipient.ts
+++ lib/packages/core/src/typings/rewardRecipient.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2020 Burst Apps Team
+ */
+
+/**
+ * Reward Recipient
+ *
+ * @module core */
 export interface RewardRecipient {
     rewardRecipient: string;
     requestProcessingTime: number;
