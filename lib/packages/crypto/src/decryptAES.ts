fd2ee3ea9b32e2d40db1616d200edf9338901148
ohager <oliver.hager@dextra-sw.com>
2021-06-02 13:00:03 -0300

    fix: malformed utf-8

--- lib/packages/crypto/src/decryptAES.ts
+++ lib/packages/crypto/src/decryptAES.ts
@@ -5,10 +5,14 @@ import * as CryptoJS from 'crypto-js';
  * @see [[encryptAES]]
  * @param encryptedBase64 encrypted data in base64 format
  * @param key The secret key
- * @return The decrypted content
+ * @return The decrypted content, or `undefined` if failed
  * @module crypto
  */
 export const decryptAES = (encryptedBase64: string, key: string): string => {
-    const decrypted = CryptoJS.AES.decrypt(encryptedBase64, key);
-    return decrypted && decrypted.toString(CryptoJS.enc.Utf8);
+    try{
+        const decrypted = CryptoJS.AES.decrypt(encryptedBase64, key);
+        return decrypted && decrypted.toString(CryptoJS.enc.Utf8);
+    }catch (e){
+        return undefined
+    }
 };
