cd7d550825afca45a2a47a977d31e708d65bdf6b
ohager <oliver.hager@dextra-sw.com>
2021-09-07 14:30:00 -0300

    fix: I fucking fixed the translation shit

--- web/angular-wallet/src/app/layout/components/i18n/i18n.module.ts
+++ web/angular-wallet/src/app/layout/components/i18n/i18n.module.ts
@@ -1,25 +1,23 @@
-import {NgModule} from "@angular/core";
+import {NgModule} from '@angular/core';
 
-import {LanguageSelectorComponent} from "./language-selector/language-selector.component";
-import {I18nPipe} from "./i18n.pipe";
-import {I18nService} from "./i18n.service";
-import {CommonModule} from "@angular/common";
+import {LanguageSelectorComponent} from './language-selector/language-selector.component';
+import {I18nPipe} from './i18n.pipe';
+import {CommonModule} from '@angular/common';
 
 export interface Language {
-  name: string,
-  code: string
+  name: string;
+  code: string;
 }
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations:[
+  declarations: [
     LanguageSelectorComponent,
     I18nPipe
   ],
   exports: [LanguageSelectorComponent, I18nPipe],
-  providers: [I18nService]
-
 })
-export class I18nModule{}
+export class I18nModule {
+}
--- web/angular-wallet/src/app/layout/components/i18n/i18n.pipe.ts
+++ web/angular-wallet/src/app/layout/components/i18n/i18n.pipe.ts
@@ -3,26 +3,14 @@ import {I18nService} from './i18n.service';
 
 @Pipe({
   name: 'i18n',
-  pure: false
+  pure: false,
 })
 export class I18nPipe implements PipeTransform {
 
-  private cachedLanguageCode: string;
-
-  constructor(public i18nService: I18nService) {
-    // this.i18nService.subscribe((data) =>
-    //   // console.log('I18nPipe', data), null
-    // );
+  constructor(private i18nService: I18nService) {
   }
-
-
-  private shouldUpdate(): boolean {
-    return this.i18nService.currentLanguage.code !== this.cachedLanguageCode;
-  }
-
 
   transform(phrase: any, args?: any): any {
-    // if (!this.shouldUpdate()) { return; }
     return this.i18nService.getTranslation(phrase);
   }
 
