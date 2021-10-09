a1f70a0164396068bdcd66b86e7f49f118ee29de
ohager <oliver.hager@dextra-sw.com>
2021-10-09 17:34:29 -0300

    feat: added forged blocks card

--- web/angular-wallet/src/app/main/dashboard/widgets/blockforged/blockforged.component.ts
+++ web/angular-wallet/src/app/main/dashboard/widgets/blockforged/blockforged.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Account, Block } from '@signumjs/core';
+import { AccountService } from '../../../../setup/account/account.service';
+import { ChainTime } from '@signumjs/util';
+import { formatCurrency } from "@angular/common";
+import { Settings } from "../../../../settings";
+import { UnsubscribeOnDestroy } from "../../../../util/UnsubscribeOnDestroy";
+import { StoreService } from "../../../../store/store.service";
+import { takeUntil } from "rxjs/operators";
+
+interface ForgedBlockInfo {
+  count: number;
+  minedIncomeSigna: number;
+  lastBlock: Block;
+}
 
 @Component({
   selector: 'app-blockforged',
   templateUrl: './blockforged.component.html',
-  styleUrls: ['./blockforged.component.scss']
+  styleUrls: ['./blockforged.component.scss', '../widget.shared.scss']
 })
-export class BlockforgedComponent implements OnInit {
+export class BlockforgedComponent extends UnsubscribeOnDestroy implements OnInit {
+  @Input() public account: Account;
+  @Input() public priceBtc: number;
+  @Input() public priceUsd: number;
+  @Input() public priceEur: number;
+  @Input() public priceRub: number;
 
-  constructor() { }
+  locale = 'en';
+  isLoading = true;
+  blockInfo: ForgedBlockInfo;
+  private interval: NodeJS.Timeout;
 
-  ngOnInit() {
+  private unsubscribe = takeUntil(this.unsubscribeAll);
+
+  constructor(
+    private accountService: AccountService,
+    private storeService: StoreService,
+  ) {
+    super();
+    this.blockInfo = {
+      count: 0,
+      lastBlock: null,
+      minedIncomeSigna: 0
+    };
   }
+
+  async ngOnInit(): Promise<void> {
+    await this.updateForgedBlocks();
+    this.isLoading = false;
+    this.interval = setInterval(
+      () => this.updateForgedBlocks(),
+      2 * 60 * 1000);
+
+    this.storeService.settings
+      .pipe(this.unsubscribe)
+      .subscribe((settings: Settings) => {
+        this.locale = settings.language;
+      });
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
+
+  private async updateForgedBlocks(): Promise<void> {
+    const { blocks } = await this.accountService.getForgedBlocks(this.account);
+    this.blockInfo = blocks.reduce<ForgedBlockInfo>((acc, b) => {
+        if (!(acc.lastBlock && acc.lastBlock.timestamp >= b.timestamp)) {
+          acc.lastBlock = b;
+        }
+        acc.minedIncomeSigna += parseInt(b.blockReward, 10);
+        acc.count++;
+        return acc;
+      },
+      {
+        minedIncomeSigna: 0,
+        lastBlock: null,
+        count: 0
+      });
+  }
+
+  getLastBlockDate(): Date | null {
+    return this.blockInfo.count > 0
+      ? ChainTime.fromChainTimestamp(this.blockInfo.lastBlock.timestamp).getDate()
+      : null;
+  }
+
+  private asCurrency(value: number, currency: string, digitsInfo: string = '1.2-2'): string {
+    return `${formatCurrency(value, this.locale, currency, '', digitsInfo)}`;
+  }
+
+  public getPriceBtc = (): string => {
+    return `฿${this.asCurrency(this.priceBtc * this.blockInfo.minedIncomeSigna, '', '1.0-4')}`;
+  }
+  public getPriceUsd = (): string => `$${this.asCurrency(this.priceUsd * this.blockInfo.minedIncomeSigna, '', '1.0-2')}`;
+  public getPriceEur = (): string => `€${this.asCurrency(this.priceEur * this.blockInfo.minedIncomeSigna, '', '1.0-2')}`;
+  public getPriceRub = (): string => `₽${this.asCurrency(this.priceRub * this.blockInfo.minedIncomeSigna, '', '1.0-2')}`;
 
 }
