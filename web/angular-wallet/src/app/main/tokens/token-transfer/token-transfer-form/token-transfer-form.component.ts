2a69b6c751f93050076b0a414d6d562aa49016da
ohager <oliver@devbutze.com>
2022-12-30 13:31:43 -0300

    fix: fixes #1664

--- web/angular-wallet/src/app/main/tokens/token-transfer/token-transfer-form/token-transfer-form.component.ts
+++ web/angular-wallet/src/app/main/tokens/token-transfer/token-transfer-form/token-transfer-form.component.ts
@@ -7,7 +7,7 @@ import {
 import { Recipient, RecipientValidationStatus } from 'app/components/recipient-input/recipient-input.component';
 import { AccountBalances, getBalancesFromAccount } from 'app/util/balance';
 import { NgForm } from '@angular/forms';
-import { Amount, ChainValue, CurrencySymbol } from "@signumjs/util";
+import { Amount, ChainValue } from "@signumjs/util";
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { WarnSendDialogComponent } from '../../../../components/warn-send-dialog/warn-send-dialog.component';
 import { asNumber, isNotEmpty } from '../../../../util/forms';
@@ -137,8 +137,7 @@ export class TokenTransferFormComponent implements OnInit {
       && isNotEmpty(this.recipient.addressRaw)
       && this.hasSufficientQuantity()
       && this.hasSufficientBalance()
-      && !this.isMessageTooLong()
-      && this.recipient.status !== RecipientValidationStatus.INVALID;
+      && !this.isMessageTooLong();
   }
 
   setPin(pin: string): void {
@@ -160,13 +159,13 @@ export class TokenTransferFormComponent implements OnInit {
       return this.i18nService.getTranslation('message_too_long');
     }
     if (this.recipient.status === RecipientValidationStatus.INVALID) {
-      return this.i18nService.getTranslation('invalid_address');
+      return this.i18nService.getTranslation('unregistered_address');
     }
     if (this.recipient.status === RecipientValidationStatus.BURN) {
       return this.i18nService.getTranslation('burn_address');
     }
     if (this.recipient.status === RecipientValidationStatus.UNKNOWN) {
-      return this.i18nService.getTranslation('unknown_address');
+      return this.i18nService.getTranslation('recipient_required');
     }
     if (Number.isNaN(this.getQuantity())) {
       return this.i18nService.getTranslation('csv_error_invalid_amount');
