3066bcd380cbcf389261f76f3bf798a60df14972
Tony Stipanic <tony@stipanic.ch>
2023-01-30 00:55:31 +0100

    feat: Replace markdown package

--- mobile/src/modules/auth/components/terms/TermsScreen.tsx
+++ mobile/src/modules/auth/components/terms/TermsScreen.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { Modal, ScrollView, StyleSheet } from "react-native";
-import Markdown from "react-native-markdown-renderer";
-import { SafeAreaView } from "react-navigation";
-import { Button } from "../../../../core/components/base/Button";
-import { Screen } from "../../../../core/layout/Screen";
-import { Colors } from "../../../../core/theme/colors";
-import { LICENSE } from "./LICENSE";
+import React from 'react';
+import {Modal, ScrollView, StyleSheet} from 'react-native';
+// @ts-ignore
+import Markdown from 'react-native-markdown-package';
+import {SafeAreaView} from 'react-navigation';
+import {Button} from '../../../../core/components/base/Button';
+import {Screen} from '../../../../core/layout/Screen';
+import {Colors} from '../../../../core/theme/colors';
+import {LICENSE} from './LICENSE';
 
 interface Props {
   visible: boolean;
@@ -15,32 +16,32 @@ interface Props {
 const styles = StyleSheet.create({
   view: {
     backgroundColor: Colors.BLUE,
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    height: "100%",
+    height: '100%',
     zIndex: 10, // must be >9
   },
   heading1: {
     fontSize: 18,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    textTransform: "uppercase",
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
   },
   heading2: {
     fontSize: 12,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   heading3: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   heading4: {
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   text: {
-    color: "#ffffff",
+    color: '#ffffff',
   },
 });
 
@@ -50,12 +51,12 @@ export class TermsScreen extends React.PureComponent<Props> {
   };
 
   render() {
-    const { visible } = this.props;
+    const {visible} = this.props;
 
     return (
       <Screen style={styles.view}>
         <SafeAreaView>
-          <ScrollView style={{ paddingLeft: 15, paddingRight: 15 }}>
+          <ScrollView style={{paddingLeft: 15, paddingRight: 15}}>
             <Markdown style={styles}>{LICENSE}</Markdown>
             <Button onPress={this.handleAgree}>Agree</Button>
           </ScrollView>
