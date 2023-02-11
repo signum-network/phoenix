2b5b3f29bbe7006ddd3f8874ecfa2b2b4563f9b8
ohager <oliver@devbutze.com>
2023-02-11 17:30:37 -0300

    chore: some minor cleanups

--- web/angular-wallet/src/app/main/dashboard/widgets/miningpool/miningpool.component.ts
+++ web/angular-wallet/src/app/main/dashboard/widgets/miningpool/miningpool.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
-import {TransactionMiningSubtype, TransactionType } from '@signumjs/core';
-import { AccountService } from '../../../../setup/account/account.service';
-import { UnsubscribeOnDestroy } from '../../../../util/UnsubscribeOnDestroy';
+import { TransactionMiningSubtype, TransactionType } from '@signumjs/core';
+import { AccountService } from 'app/setup/account/account.service';
+import { UnsubscribeOnDestroy } from 'app/util/UnsubscribeOnDestroy';
 import { ChainTime } from '@signumjs/util';
 import { WalletAccount } from 'app/util/WalletAccount';
 
@@ -32,25 +32,25 @@ export class MiningpoolComponent extends UnsubscribeOnDestroy implements OnInit,
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.account && changes.account.previousValue !== changes.account.currentValue){
+    if (changes.account && changes.account.previousValue !== changes.account.currentValue) {
       this.fetchPoolAccount();
     }
   }
-  private async fetchPoolAccount(): Promise<void> {
-    try{
 
-    const rewardRecipient = await this.accountService.getRewardRecipient(this.account.account);
-    this.poolAccount = rewardRecipient.account !== this.account.account ? rewardRecipient : null;
-    if (this.poolAccount) {
-      this.poolName = this.getPoolUrlOrName();
-      const assignments = await this.accountService.getAccountTransactions({
-        accountId: this.account.account,
-        type: TransactionType.Mining,
-        subtype: TransactionMiningSubtype.RewardRecipientAssignment
-      });
-      this.lastPoolAssignment = ChainTime.fromChainTimestamp(assignments.transactions[0].timestamp).getDate();
-    }
-    }catch (e){
+  private async fetchPoolAccount(): Promise<void> {
+    try {
+      const rewardRecipient = await this.accountService.getRewardRecipient(this.account.account);
+      this.poolAccount = rewardRecipient.account !== this.account.account ? rewardRecipient : null;
+      if (this.poolAccount) {
+        this.poolName = this.getPoolUrlOrName();
+        const assignments = await this.accountService.getAccountTransactions({
+          accountId: this.account.account,
+          type: TransactionType.Mining,
+          subtype: TransactionMiningSubtype.RewardRecipientAssignment
+        });
+        this.lastPoolAssignment = ChainTime.fromChainTimestamp(assignments.transactions[0].timestamp).getDate();
+      }
+    } catch (e) {
       console.warn('MiningPool Component', e.message);
     }
   }
